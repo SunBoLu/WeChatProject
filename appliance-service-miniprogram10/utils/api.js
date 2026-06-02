db8eca23fcd8abb70c1652d587a0453445abe5d0
SunBoLu <1067448736@qq.com>
2026-06-02 10:55:49 +0800

    Update api.js

--- appliance-service-miniprogram10/utils/api.js
+++ appliance-service-miniprogram10/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://appliance-service-api.onrender.com';
 
 const request = (url, method, data = {}) => {
   return new Promise((resolve, reject) => {
